dc7399d86c20e0840e4fb49f8a279fb6d0a7b4f9
elenaa77 <152157990+elenaa77@users.noreply.github.com>
2025-03-28 10:48:01 +0200

    Add files via upload

---
+++ 2lapa.js
@@ -0,0 +1,30 @@
+// Данные о турах
+document.addEventListener("DOMContentLoaded", function () {
+    const piedavajumi = [
+        { valsts: "Ņujorka", cena: 1200, attels: "" },
+        { valsts: "Losandželosa", cena: 1500, attels: "" },
+        { valsts: "Maijami", cena: 1800, attels: "" },
+        { valsts: "Čikāga", cena: 2000, attels: "" }
+    ];
+ 
+    function raditPiedavajumus(filtrEtiePiedavajumi = piedavajumi) {
+        const piedavajumuKonteiners = document.getElementById("piedavajumi");
+ 
+       
+        piedavajumuKonteiners.innerHTML = "";// Notīra iepriekšējo saturu
+       
+        filtrEtiePiedavajumi.forEach(piedavajums => { //Metode .forEach() pārskata (iet cauri) masīvam filtrEtiePiedavajumi un izveido HTML elementus katram piedāvājumam.
+            const piedavajumaBloks = document.createElement("div");
+            piedavajumaBloks.classList.add("piedavajuma-kartite");
+            piedavajumaBloks.innerHTML = `
+                <img src="${piedavajums.attels}" alt="${piedavajums.valsts}">
+                <h3>${piedavajums.valsts}</h3>
+                <p>Cena: ${piedavajums.cena}$</p>
+            `;
+            piedavajumuKonteiners.appendChild(piedavajumaBloks);// Pievieno izveidoto bloku HTML lapā
+        });
+    }
+    raditPiedavajumus();//izsauc funkciju
+});
+
+
